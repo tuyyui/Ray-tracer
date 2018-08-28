f62efb1983f3abeb965e4399933d3b7a926a2011
ChinksofLight <ounane.farouk@gmail.com>
2018-08-28 17:03:48 +0100

    Update sphere.h

--- sphere.h
+++ sphere.h
@@ -11,16 +11,6 @@ public:
         Radius = radius;
     }
     bool intersect(Vec3 &rayorig, Vec3 &raydir, float &t0, float &t1) {
-
-        Vec3 l = center - rayorig;
-        float tca = l.dot(raydir);
-        if(tca < 0) return false;
-        float d2 = l.dot(l) - tca * tca;
-        if(d2 > radius2) return false;
-        float thc =sqrt(radius - d2);
-        t0 = tca - thc;
-        t1 = tca + thc;
-
-        return true;
+     
     }
 };
