3f977457d497f629efd469ca7d64159c5564e3aa
projekcja <50920675+projekcja@users.noreply.github.com>
2019-05-23 18:58:46 +0300

    Add files via upload

--- color.h
+++ color.h
@@ -7,14 +7,15 @@ class Color_t  {
 public:
 	int r, g, b;
 	Color_t(int red, int green, int blue) : r(red), g(green), b(blue) {}
+	
 	Color_t scale_by(double scalar)const {
-		return (scalar > 0) ? Color_t(scalar * r, scalar * g, scalar * b) : Color_t(0,0,0);
+		return (scalar > 0) ? Color_trunc(scalar * r, scalar * g, scalar * b) : Color_t(0,0,0);
 	}
 	Color_t scale_by2(double scalar)const {
 		if (scalar < 0)
 			return  Color_t(0, 0, 0);
 		scalar *= scalar;
-		return Color_t(scalar * r, scalar * g, scalar * b);
+		return Color_trunc(scalar * r, scalar * g, scalar * b);
 	}
 	Color_t mix_with(const Color_t& rhs) const {
 		return Color_t(r * rhs.r, g * rhs.g, b * rhs.b);
@@ -23,10 +24,13 @@ public:
 		return Color_t(scalar * r, scalar * g, scalar * b);
 	}
 	Color_t operator +(const Color_t& rhs) const {
-		return Color_t(trunc(r + rhs.r), trunc(g + rhs.g), trunc(b + rhs.b));
+		return Color_trunc(r + rhs.r, g + rhs.g, b + rhs.b);
 	}
 	static int trunc(int val) {
 		return (val > 255) ? 255 : val;
+	}
+	static Color_t Color_trunc(int red, int green, int blue) {
+		return Color_t(trunc(red), trunc(green), trunc(blue));
 	}
 };
 
